fb45acbc0713be648980317e0cee6bff1b201e78
Vinh Duong <vinh@localhost.localdomain>
2020-04-06 01:23:03 +0700

    refactor

--- README.md
+++ README.md
@@ -13,27 +13,30 @@ r := router.New()
 
 api := r.Group("/api")
 
-api.Use(func(c *router.Context) {
-	c.Response.Header.Set("Content-Type", "application/json")
+api.Use(func(ctx *fasthttp.RequestCtx) bool {
+  ctx.Response.Header.Set("Content-Type", "application/json")
+  return false
 })
 
 apiv1 := api.Group("/v1")
 
-apiv1.Use(func(c *router.Context) {
-	c.Response.Header.Set("X-Hello", "Hello") // dont trust the client
-	c.SetUserValue("user", map[string]string{"Name": "Guest"})
+apiv1.Use(func(ctx *fasthttp.RequestCtx) bool {
+  ctx.Response.Header.Set("X-Hello", "Hello") // dont trust the client
+  ctx.SetUserValue("user", map[string]string{"Name": "Guest"})
+  return false
 })
 
 // apiv1.Get
 
-apiv1.Any("/", func(c *router.Context) {
-	user, ok := c.UserValue("user").(map[string]string)
-	name, ok := user["Name"]
-	if !ok {
-		c.Error("Error", fasthttp.StatusInternalServerError)
-		return
-	}
-	c.SetBodyString("\"Hello " + name + "\"")
+apiv1.Any("/", func(ctx *fasthttp.RequestCtx) bool {
+  user, ok := ctx.UserValue("user").(map[string]string)
+  name, ok := user["Name"]
+  if !ok {
+    ctx.Error("Error", fasthttp.StatusInternalServerError)
+    return true
+  }
+  ctx.SetBodyString("\"Hello " + name + "\"")
+  return false
 })
 ```
 Checkout [Example](example/main.go)
@@ -46,10 +49,5 @@ Checkout [Example](example/main.go)
   - r.NotFound : Set not found function
   - r.MethodNotAllowed : Set MethodNotAllowed function
   - r.OnError : Set panic handler function 
-
-  - c.Abort : Abort next handler
-  - c.SetData : (Removed) use SetUserValue instead
-  - c.GetData : (Removed) use UserValue instead
-
 #### License
 - MIT
--- example/main.go
+++ example/main.go
@@ -1,105 +1,65 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
-	"os"
-	"runtime"
 	"strconv"
-	"strings"
 
 	"github.com/valyala/fasthttp"
-	"github.com/vinhjaxt/fasthttp-staticrouter"
+	router "github.com/vinhjaxt/fasthttp-staticrouter"
 )
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	port := flag.Int("port", 8080, "HTTP port")
+	flag.Parse()
 
 	r := router.New()
 
 	api := r.Group("/api")
 
-	api.Use(func(c *router.Context) {
-		c.Response.Header.Set("Content-Type", "application/json")
+	api.Use(func(ctx *fasthttp.RequestCtx) bool {
+		ctx.Response.Header.Set("Content-Type", "application/json")
+		return false
 	})
 
 	apiv1 := api.Group("/v1")
 
-	apiv1.Use(func(c *router.Context) {
-		c.Response.Header.Set("X-Hello", "Hello") // dont trust the client
-		c.SetUserValue("user", map[string]string{"Name": "Guest"})
+	apiv1.Use(func(ctx *fasthttp.RequestCtx) bool {
+		ctx.Response.Header.Set("X-Hello", "Hello") // dont trust the client
+		ctx.SetUserValue("user", map[string]string{"Name": "Guest"})
+		return false
 	})
 
 	// apiv1.Get
 
-	apiv1.Any("/", func(c *router.Context) {
-		user, ok := c.UserValue("user").(map[string]string)
+	apiv1.Any("/", func(ctx *fasthttp.RequestCtx) bool {
+		user, ok := ctx.UserValue("user").(map[string]string)
 		name, ok := user["Name"]
 		if !ok {
-			c.Error("Error", fasthttp.StatusInternalServerError)
-			return
+			ctx.Error("Error", fasthttp.StatusInternalServerError)
+			return true
 		}
-		c.SetBodyString("\"Hello " + name + "\"")
+		ctx.SetBodyString("\"Hello " + name + "\"")
+		return false
 	})
 
 	fs := &fasthttp.FS{
 		Root:               "./public_web",
-		IndexNames:         []string{"index.html", "index.htm"},
+		IndexNames:         []string{"index.html"},
 		AcceptByteRange:    true,
 		GenerateIndexPages: false,
 		Compress:           true,
 		// CacheDuration: ,
 	}
-	FSHandler := fs.NewRequestHandler()
-	r.NotFound(func(c *router.Context) {
-		FSHandler(c.RequestCtx)
-	})
-	requestHandler := r.BuildHandler()
+	r.NotFound(fs.NewRequestHandler())
+
 	api = nil
 	apiv1 = nil
-	r = nil
 
-	go func() {
-		HTTPPort, err := strconv.Atoi(strings.Trim(os.Getenv("GO_APP_HTTP_PORT"), " \r\n\t"))
-		if err != nil {
-			HTTPPort = 80
-		}
-		// Start HTTP server.
-		HTTPServer := &fasthttp.Server{
-			Handler:              requestHandler,
-			Name:                 "nginx",
-			ReadTimeout:          120 * 1000000000, // 120s
-			WriteTimeout:         120 * 1000000000,
-			MaxKeepaliveDuration: 120 * 1000000000,
-			MaxRequestBodySize:   2 * 1048576, // 2MB
-		}
-		log.Printf("\r\nHTTP Server running on port %d", HTTPPort)
-		if err := HTTPServer.ListenAndServe(fmt.Sprintf(":%d", HTTPPort)); err != nil {
-			log.Fatalf("error in ListenAndServe: %s", err)
-		}
-	}()
-
-	go func() {
-		HTTPSPort, err := strconv.Atoi(strings.Trim(os.Getenv("GO_APP_HTTPS_PORT"), " \r\n\t"))
-		if err == nil {
-			// Start HTTPS server.
-			HTTPSServer := &fasthttp.Server{
-				Handler:              requestHandler,
-				Name:                 "nginx",
-				ReadTimeout:          120 * 1000000000, // 120s
-				WriteTimeout:         120 * 1000000000,
-				MaxKeepaliveDuration: 120 * 1000000000,
-				MaxRequestBodySize:   2 * 1048576, // 2MB
-			}
-			certFile := ""
-			keyFile := ""
-			log.Printf("Starting HTTPS server on port %d", HTTPSPort)
-			if err := HTTPSServer.ListenAndServeTLS(fmt.Sprintf(":%d", HTTPSPort), certFile, keyFile); err != nil {
-				log.Fatalf("error in ListenAndServeTLS: %s", err)
-			}
-		}
-	}()
-
-	// Wait forever.
-	select {}
+	// Start HTTP server.
+	s := &fasthttp.Server{
+		Handler: r.BuildHandler(),
+	}
+	log.Println("Server running on", *port)
+	log.Panicln(s.ListenAndServe(":" + strconv.Itoa(*port)))
 }
